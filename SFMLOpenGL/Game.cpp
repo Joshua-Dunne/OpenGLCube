cd46370866838077b354673609bd9148bc4b204d
Joshua Dunne <C00241588@itcarlow.ie>
2020-01-07 11:55:25 +0000

    Fixed winding for faces so now the cube displays correctly. Changed each triangle to be a different color on each face, so both triangles that make up one face can be seen clearly.

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -126,80 +126,13 @@ void Game::initialize()
 	// Creates a new Display List
 	// Initalizes and Compiled to GPU
 	// https://www.opengl.org/sdk/docs/man2/xhtml/glNewList.xml
-	glNewList(index, GL_COMPILE);
-	glBegin(GL_TRIANGLES);
-	{
-		//Front Face
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-
-		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-
-		//Back Face
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-		glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
-		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-
-		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-		glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-
-		////Top Face
-		//glColor3f(1.0f, 0.0f, 0.0f);
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-		//glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-		//glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-
-		////Bottom Face
-		//glColor3f(1.0f, 0.0f, 0.0f);
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-
-		////Left Face
-		//glColor3f(1.0f, 1.0f, 0.0f);
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-		//glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-
-		////Right Face
-		//glColor3f(1.0f, 1.0f, 0.0f);
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-		//glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-
-		////Complete the faces of the Cube
-	}
-	glEnd();
-	glEndList();
 }
 
 void Game::update()
 {
 	cout << "Update up" << endl;
 
-	glDeleteLists(0, 1);
+	//glDeleteLists(0, 1);
 	updateGL();
 }
 
@@ -230,64 +163,71 @@ void Game::updateGL()
 	glBegin(GL_TRIANGLES);
 	{
 		//Front Face
-		glColor3f(0.0f, 0.0f, 1.0f);
+		glColor3f(1.0f, 0.0f, 1.0f);
 		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
 		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
 		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
 
+		glColor3f(1.0f, 0.5f, 1.0f);
 		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
 		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
 		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
 
 		//Back Face
 		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
 		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
+		glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
+
+		glColor3f(0.0f, 1.0f, 0.5f);
+		glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
+		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
+		glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
+
+		//Top Face
+		glColor3f(1.0f, 0.5f, 0.0f);
+		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
+		glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
+		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
+		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
+		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
+
+		//Bottom Face
+		glColor3f(1.0f, 0.0f, 0.5f);
+		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
+		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
+		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
+		glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
+		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
+		
+
+		//Left Face
+		glColor3f(0.0f, 1.0f, 1.0f);
+		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
 		glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
 		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
 
+		glColor3f(0.5f, 1.0f, 1.0f);
 		glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
+		glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
+		glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
+
+		//Right Face
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
+		glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
+		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
+
+		glColor3f(1.0f, 1.0f, 0.5f);
+		glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
 		glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
 		glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-
-		////Top Face
-		//glColor3f(1.0f, 0.0f, 0.0f);
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-		//glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-		//glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-
-		////Bottom Face
-		//glColor3f(1.0f, 0.0f, 0.0f);
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-
-		////Left Face
-		//glColor3f(1.0f, 1.0f, 0.0f);
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-		//glVertex3f(points[7].getX(), points[7].getY(), points[7].getZ());
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-
-		//glVertex3f(points[4].getX(), points[4].getY(), points[4].getZ());
-		//glVertex3f(points[0].getX(), points[0].getY(), points[0].getZ());
-		//glVertex3f(points[3].getX(), points[3].getY(), points[3].getZ());
-
-		////Right Face
-		//glColor3f(1.0f, 1.0f, 0.0f);
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
-		//glVertex3f(points[2].getX(), points[2].getY(), points[2].getZ());
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-
-		//glVertex3f(points[1].getX(), points[1].getY(), points[1].getZ());
-		//glVertex3f(points[5].getX(), points[5].getY(), points[5].getZ());
-		//glVertex3f(points[6].getX(), points[6].getY(), points[6].getZ());
 
 		//Complete the faces of the Cube
 	}
